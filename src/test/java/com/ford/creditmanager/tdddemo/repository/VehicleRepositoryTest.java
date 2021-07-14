383e7a8463293d4e760567972729464754addf29
Basi Yonah Bwambale <basiyonah@gmail.com>
2021-07-13 23:12:36 -0400

    Add Interfaces and high level Project Structure

--- src/test/java/com/ford/creditmanager/tdddemo/repository/VehicleRepositoryTest.java
+++ src/test/java/com/ford/creditmanager/tdddemo/repository/VehicleRepositoryTest.java
@@ -1,13 +1,23 @@
 package com.ford.creditmanager.tdddemo.repository;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
 class VehicleRepositoryTest {
+    private VehicleRepository vehicleRepository;
+
+    @Autowired
+    public VehicleRepositoryTest(VehicleRepository repo){
+        this.vehicleRepository = repo;
+    }
 
     @Test
     void findAll() {
+        assertEquals(0, vehicleRepository.findAll().size());
     }
 
     @Test
