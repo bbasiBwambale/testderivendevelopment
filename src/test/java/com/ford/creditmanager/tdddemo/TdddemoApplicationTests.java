58ffe88b535e03b5310b5f8e7a835c51f82bc39e
Basi Yonah Bwambale <basiyonah@gmail.com>
2021-07-13 20:49:41 -0400

    Initial Project Commit

---
+++ src/test/java/com/ford/creditmanager/tdddemo/TdddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ford.creditmanager.tdddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdddemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
