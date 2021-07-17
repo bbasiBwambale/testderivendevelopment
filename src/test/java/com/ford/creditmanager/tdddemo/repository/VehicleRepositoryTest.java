502c9555bde53a10b7c9afdcec1a1891db44b868
Basi Yonah Bwambale <basiyonah@gmail.com>
2021-07-17 18:37:06 -0400

    Enable H2, Add initial data, add two car objects in RepositoryTest

--- src/test/java/com/ford/creditmanager/tdddemo/repository/VehicleRepositoryTest.java
+++ src/test/java/com/ford/creditmanager/tdddemo/repository/VehicleRepositoryTest.java
@@ -1,14 +1,28 @@
 package com.ford.creditmanager.tdddemo.repository;
 
+import com.ford.creditmanager.tdddemo.domain.Vehicle;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 class VehicleRepositoryTest {
+    @Autowired
     private VehicleRepository vehicleRepository;
+
+    private Vehicle car;
+    private Vehicle updatedCar;
+
+    @BeforeEach
+    public void setUp(){
+        car = Vehicle.builder().vin(UUID.randomUUID()).model("Escape 2020").build();
+        updatedCar = Vehicle.builder().vin(UUID.randomUUID()).model("Machi-ev 2021").build();
+    }
 
     @Autowired
     public VehicleRepositoryTest(VehicleRepository repo){
