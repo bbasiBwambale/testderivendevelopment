1f0f5675d49763ab0f58fdc07f911b08c7de3119
Basi Yonah Bwambale <basiyonah@gmail.com>
2021-07-17 20:10:29 -0400

    Add Vehicle Service Tests and Implimentation

--- src/main/java/com/ford/creditmanager/tdddemo/service/VehicleServiceImpl.java
+++ src/main/java/com/ford/creditmanager/tdddemo/service/VehicleServiceImpl.java
@@ -1,33 +1,42 @@
 package com.ford.creditmanager.tdddemo.service;
 
 import com.ford.creditmanager.tdddemo.domain.Vehicle;
+import com.ford.creditmanager.tdddemo.repository.VehicleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
 
-public class VehicleServiceImpl implements IVehicleService{
+@Service
+
+public class VehicleServiceImpl implements IVehicleService {
+    @Autowired
+    private VehicleRepository vehicleRepository;
+
     @Override
     public List<Vehicle> findAll() {
-        return null;
+        return vehicleRepository.findAll();
     }
 
     @Override
     public Vehicle findById(UUID vin) {
-        return null;
+        return vehicleRepository.findById(vin).get();
     }
 
     @Override
     public Vehicle update(UUID vin, Vehicle vehicle) {
-        return null;
+        vehicle.setVin(vin);
+        return vehicleRepository.save(vehicle);
     }
 
     @Override
     public Vehicle save(Vehicle vehicle) {
-        return null;
+        return vehicleRepository.save(vehicle);
     }
 
     @Override
     public void deleteById(UUID vin) {
-
+        vehicleRepository.deleteById(vin);
     }
 }
