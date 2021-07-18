29f77a33b88705229b07d584e8293d3c1853c0e5
Basi Yonah Bwambale <basiyonah@gmail.com>
2021-07-17 20:59:34 -0400

    Add vehicle controller Tests and Implimentation

--- src/main/java/com/ford/creditmanager/tdddemo/controller/VehicleControllerImpl.java
+++ src/main/java/com/ford/creditmanager/tdddemo/controller/VehicleControllerImpl.java
@@ -1,33 +1,47 @@
 package com.ford.creditmanager.tdddemo.controller;
 
 import com.ford.creditmanager.tdddemo.domain.Vehicle;
+import com.ford.creditmanager.tdddemo.service.VehicleServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
+@RestController
+@RequestMapping("/vehicles")
 public class VehicleControllerImpl implements IVehicleController{
+    @Autowired
+    private VehicleServiceImpl vehicleService;
+
     @Override
+    @GetMapping("/")
     public ResponseEntity findAll() {
-        return null;
+        return new ResponseEntity(vehicleService.findAll(), HttpStatus.OK);
     }
 
+    @GetMapping("/{vin}")
     @Override
-    public ResponseEntity findById(UUID vin) {
-        return null;
+    public ResponseEntity findById(@PathVariable UUID vin) {
+        return new ResponseEntity(vehicleService.findById(vin), HttpStatus.OK);
     }
 
+    @PutMapping("/{vin}")
     @Override
-    public ResponseEntity update(UUID vin, Vehicle vehicle) {
-        return null;
+    public ResponseEntity update(@PathVariable UUID vin, @RequestBody Vehicle vehicle) {
+        return new ResponseEntity(vehicleService.update(vin, vehicle), HttpStatus.CREATED);
     }
 
+    @PostMapping("/")
     @Override
-    public ResponseEntity save(Vehicle vehicle) {
-        return null;
+    public ResponseEntity save(@RequestBody Vehicle vehicle) {
+        return new ResponseEntity(vehicleService.save(vehicle), HttpStatus.CREATED);
     }
 
+    @DeleteMapping("/{vin}")
     @Override
-    public void deleteById(UUID vin) {
-
+    public void deleteById(@PathVariable UUID vin) {
+        vehicleService.deleteById(vin);
     }
 }
