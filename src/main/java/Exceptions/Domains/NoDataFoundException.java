9dfe0878f2d9005a2d2195c7386d2122c7531eb6
Basi Yonah Bwambale <basiyonah@gmail.com>
2021-08-12 08:39:06 -0400

    Added Exception Handling

---
+++ src/main/java/Exceptions/Domains/NoDataFoundException.java
@@ -0,0 +1,4 @@
+package Exceptions.Domains;
+
+public class NoDataFoundException extends Exception{
+}
---
+++ src/main/java/Exceptions/Domains/VehicleNotFoundException.java
@@ -0,0 +1,4 @@
+package Exceptions.Domains;
+
+public class VehicleNotFoundException extends Exception{
+}
